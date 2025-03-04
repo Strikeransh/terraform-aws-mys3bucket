97b822dd2328e691919cf9bc6d25d13ba4b8b118
Strikeransh <ashish.35794@gmail.com>
2025-03-04 10:15:57 +0000

    Add files via upload

--- resource_s3.tf
+++ resource_s3.tf
@@ -1,5 +1,7 @@
-resource "aws_s3_bucket" "bucket1" {
-bucket = "ashish-bucket-04mar2025"
+############### aws s3 resource ##############
+resource "aws_s3_bucket" "mybucket" {
+bucket = var.s3_bucket_name
+tags = {
+env = lookup(var.s3_tags, "environment")
 }
-
-
+}
